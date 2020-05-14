6fe54422ad59d93f4a1f7c2adac19400edd0c444
srg-1999 <53526793+srg-1999@users.noreply.github.com>
2020-05-14 10:50:59 +0530

    Add files via upload

--- median_3.py
+++ median_3.py
@@ -1,44 +1,45 @@
 def median_3(arr1,arr2):
     n = len(arr1) + len(arr2)
-    i,j,k=0,0,0
+    i,j,counter=0,0,0
     a,b=0,0
     if (n)%2==1:
-        if k<=n//2+1:
+        if counter<=n//2+1:
             while i<len(arr1) and j<len(arr2):
                 if arr1[i]<arr2[j]:
                     i=i+1
-                    k=k+1
-                    if k == n//2+1:
+                    counter=counter+1
+                    if counter == n//2+1:
                         return arr1[i-1]
+
 
                 elif arr2[j] < arr1[i]:
                     j = j + 1
-                    k = k + 1
-                    if k==n//2+1:
+                    counter = counter + 1
+                    if counter==n//2+1:
                         return arr2[j-1]
             while i<len(arr1):
                 i=i+1
-                k=k+1
-                if k == n//2+1:
+                counter=counter+1
+                if counter == n//2+1:
                     return arr1[i-1]
 
             while j<len(arr2):
 
                 j=j+1
-                k=k+1
-                if k == n//2+1:
+                counter=counter+1
+                if counter == n//2+1:
                     return arr2[j-1]
 
 
     if (n)%2==0:
-        if k<n//2+1:
+        if counter<n//2+1:
             while i<len(arr1) and j<len(arr2):
                 if arr1[i]<arr2[j]:
                     i=i+1
-                    k=k+1
-                    if k == n//2:
+                    counter=counter+1
+                    if counter == n//2:
                         a= arr1[i-1]
-                    if k == n//2+1:
+                    if counter == n//2+1:
                         b= arr1[i-1]
                         break
 
@@ -46,33 +47,38 @@ def median_3(arr1,arr2):
 
                 elif arr2[j] < arr1[i]:
                     j = j + 1
-                    k = k + 1
-                    if k==n//2:
+                    counter = counter + 1
+                    if counter==n//2:
                         a= arr2[j-1]
-                    if k==n//2+1:
+                    if counter==n//2+1:
                         b= arr2[j-1]
                         break
             while i<len(arr1):
                 i = i + 1
-                k = k + 1
-                if k == n // 2:
+                counter = counter + 1
+                if counter == n // 2:
                     a = arr1[i - 1]
-                if k == n // 2 + 1:
+                if counter == n // 2 + 1:
                     b = arr1[i - 1]
                     break
             while j<len(arr2):
                 j = j + 1
-                k = k + 1
-                if k == n // 2:
+                counter = counter + 1
+                if counter == n // 2:
                     a = arr2[j - 1]
-                if k == n // 2 + 1:
+                if counter == n // 2 + 1:
                     b = arr2[j - 1]
                     break
 
             return (a+b)/2
 
 
-arr1=[]
-arr2=[]
+arr1=[1,3,4,7,8,10]
+arr2=[2,5,9,11]
+
 result=median_3(arr1,arr2)
-print(result)
+assert result==6.0
+ar1 = [1]  # 15
+ar2 = [2]
+result=median_3(ar1,ar2)
+assert result==1.5
