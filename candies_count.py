ae316b3d332f8adc5f7b5842953d1c890aed13ac
srg-1999 <53526793+srg-1999@users.noreply.github.com>
2020-05-28 19:11:18 +0530

    Add files via upload

--- candies_count.py
+++ candies_count.py
@@ -1,5 +1,5 @@
-def candies_count(a,b):
-    x=((b*2)**(1/2))
+def candies_count(children, candies):
+    x=((candies * 2) ** (1 / 2))
     if x>int(x)+0.5:
         x=int(x)
         s=x
@@ -8,21 +8,21 @@ def candies_count(a,b):
         s=x-1
 
     r=(s*(s+1))/2
-    t=b-r#extra chocolate
-    y=s//a#times
-    m=s%a#extra
-    l=[0]*a
+    t= candies - r#extra chocolate
+    y= s // children#times
+    m= s % children#extra student
+    l= [0] * children
     print(l)
 
-    for i in range(a):
-        l[i]=y*(i+1)+((y*(y-1))/2)*a
+    for i in range(children):
+        l[i]=y*(i+1)+ ((y*(y-1))/2) * children
 
     for j in range (m):
 
-        l[j]+=y*a+j+1
+        l[j]+= y * children + j + 1
 
     l[m]+=t
 
     return l
-result=candies_count(1000,1000)
+result=candies_count(1000,4)
 print(result)
