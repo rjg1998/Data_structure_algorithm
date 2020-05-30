641b9d87c20a94a8426853a3be9918c26e2bcccc
srg-1999 <53526793+srg-1999@users.noreply.github.com>
2020-05-30 18:54:34 +0530

    Update candies_count.py

--- candies_count.py
+++ candies_count.py
@@ -1,27 +1,30 @@
 def candies_count(children, candies):
     x=((candies * 2) ** (1 / 2))
+    #s is total no. of times chocolate will be distributed
     if x>int(x)+0.5:
         x=int(x)
         s=x
     else:
         x=int(x)
         s=x-1
-
+    # r is total chocolates in exact amount
     r=(s*(s+1))/2
-    t= candies - r#extra chocolate
+    t= candies - r#extra chocolate after exact amount
+    # y is no. of exact rounds 
     y= s // children#times
+    # m is no. of students after exact rounds 
     m= s % children#extra student
     l= [0] * children
     print(l)
 
     for i in range(children):
-        l[i]=y*(i+1)+ ((y*(y-1))/2) * children
+        l[i]=y*(i+1)+ ((y*(y-1))/2) * children# sum of ap in exact rounds
 
     for j in range (m):
 
-        l[j]+= y * children + j + 1
+        l[j]+= y * children + j + 1# leftover students in last round
 
-    l[m]+=t
+    l[m]+=t# extra chocolate to last student
 
     return l
 result=candies_count(1000,4)
